89b3784b7a6722f46b962325df7fc4a87210087c
lucas beat <clashbruno10@gmail.com>
2019-12-29 21:48:29 -0300

    ok

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //variaveis
 const Discord = require('discord.js')
-const bot = new Discord.Client()
+const bot = new Discord.Client({ disableEveryone: true })
 const { GOOGLE_KEY, DISCORD_TOKENS } = require('./config')
 const prefix = '/'
 const ytdl = require('ytdl-core')
 const Youtube = require('simple-youtube-api')
 const youtube = new Youtube(GOOGLE_KEY)
+const cheerio = require('cheerio')
+const request = require('request')
+const moment = require('moment')
+require('moment-duration-format')
 let pronto = false
 var version = '1.0'
 ////////////////////////////////////////
@@ -40,6 +44,8 @@ bot.on('message', (msg) => {
     }
 })
 
+
+/////// MENSAGENS ///////
 bot.on('message', (msg) => {
     if (msg.content === `${prefix}eae`) {
         msg.reply('eae')
@@ -53,9 +59,45 @@ bot.on('message', (msg) => {
 
 bot.on('message', (msg) => {
     if (msg.content === `${prefix}tranquilo?`) {
-        msg.reply('to nao guei, vai tomar no cu')
+        msg.reply('tudo bem, e vc?')
     }
 })
+///////////////
+bot.on('message', (msg) => {
+    let args = msg.content.substring(prefix.length).split(" ")
+
+    switch (args[0]) {
+        case 'fofo':
+            imagem(msg)
+            break;
+    }
+})
+function imagem(msg) {
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "cat",
+        method: "GET",
+        headers: {
+            "accept": "text/html",
+            "user-agent": "chrome"
+        }
+    }
+    request(options, function (error, response, responseBody) {
+        if (error) {
+            return
+        }
+
+        $ = cheerio.load(responseBody)
+        var links = $(".image a.link")
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"))
+
+        console.log(urls)
+        if (!urls.length) {
+            return
+        }
+        msg.channel.send(urls[Math.floor(Math.random() * urls.length)])
+    })
+}
+
 
 //incia o bot
 bot.on('ready', () => {
